9815d42ec43af129eb02691169b547b72329347c
Guillaume <guillaume.jacquart@abtasty.com>
2022-10-28 17:59:22 +0200

    Simplify footer and improve login

--- client/components/layout/Footer.tsx
+++ client/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { SITE_INFOS } from "../../common/constants";
 
 const FooterLink = ({ label, href }: { label: string; href: string }) => (
   <Link href={href}>
-    <a className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">
+    <a className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
       {label}
     </a>
   </Link>
@@ -11,60 +11,47 @@ const FooterLink = ({ label, href }: { label: string; href: string }) => (
 
 function Footer() {
   return (
-    <div className="relative mt-16 bg-deep-purple-accent-400">
-      <svg
-        className="absolute top-0 w-full h-6 -mt-5 sm:-mt-10 sm:h-16 text-deep-purple-accent-400"
-        preserveAspectRatio="none"
-        viewBox="0 0 1440 54"
-      >
-        <path
-          fill="currentColor"
-          d="M0 22L120 16.7C240 11 480 1.00001 720 0.700012C960 1.00001 1200 11 1320 16.7L1440 22V54H1320C1200 54 960 54 720 54C480 54 240 54 120 54H0V22Z"
-        />
-      </svg>
-      <div className="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-        <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
-          <div className="md:max-w-md lg:col-span-2">
-            <Link href="/" aria-label="Go home" title="Company">
-              <a className="inline-flex items-center">
-                <svg
-                  className="w-8 text-teal-accent-400"
-                  viewBox="0 0 24 24"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeMiterlimit="10"
-                  stroke="currentColor"
-                  fill="none"
-                >
-                  <rect x="3" y="1" width="7" height="12" />
-                  <rect x="3" y="17" width="7" height="6" />
-                  <rect x="14" y="1" width="7" height="6" />
-                  <rect x="14" y="11" width="7" height="12" />
-                </svg>
-                <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
-                  {SITE_INFOS.title}
-                </span>
-              </a>
-            </Link>
-            <div className="mt-4 lg:max-w-sm">
-              <p className="text-sm text-deep-purple-50">
-                {SITE_INFOS.subtitle}
-              </p>
-            </div>
+    <div className="bg-gray-100 px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
+        <div className="md:max-w-md lg:col-span-2">
+          <Link href="/" aria-label="Go home" title="Company">
+            <a className="inline-flex items-center">
+              <svg
+                className="w-8 text-teal-accent-400"
+                viewBox="0 0 24 24"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                stroke="currentColor"
+                fill="none"
+              >
+                <rect x="3" y="1" width="7" height="12" />
+                <rect x="3" y="17" width="7" height="6" />
+                <rect x="14" y="1" width="7" height="6" />
+                <rect x="14" y="11" width="7" height="12" />
+              </svg>
+              <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                {SITE_INFOS.title}
+              </span>
+            </a>
+          </Link>
+          <div className="mt-4 lg:max-w-sm">
+            <p className="text-sm text-gray-800">{SITE_INFOS.subtitle}</p>
           </div>
-          <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
-            <div>
-              <p className="font-semibold tracking-wide text-teal-accent-400">
-                Get started
-              </p>
-              <ul className="mt-2 space-y-2">
-                <li>
-                  <FooterLink label="Sign in now" href="/api/auth/signin" />
-                </li>
-              </ul>
-            </div>
-            {/* <div>
+        </div>
+        <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
+          <div>
+            <p className="font-semibold tracking-wide text-gray-800">
+              Get started
+            </p>
+            <ul className="mt-2 space-y-2">
+              <li>
+                <FooterLink label="Sign in now" href="/api/auth/signin" />
+              </li>
+            </ul>
+          </div>
+          {/* <div>
               <p className="font-semibold tracking-wide text-teal-accent-400">
                 Cherry
               </p>
@@ -127,36 +114,35 @@ function Footer() {
                 </li>
               </ul>
             </div> */}
-          </div>
         </div>
-        <div className="flex flex-col justify-between pt-5 pb-10 border-t border-deep-purple-accent-200 sm:flex-row">
-          <p className="text-sm text-gray-100">
-            © Copyright 2022 {SITE_INFOS.title}. All rights reserved.
-          </p>
-          <div className="flex items-center mt-4 space-x-4 sm:mt-0">
-            <Link href="/">
-              <a className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400">
-                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                  <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
-                </svg>
-              </a>
-            </Link>
-            <Link href="/">
-              <a className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400">
-                <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
-                  <circle cx="15" cy="15" r="4" />
-                  <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
-                </svg>
-              </a>
-            </Link>
-            <Link href="/">
-              <a className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400">
-                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                  <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
-                </svg>
-              </a>
-            </Link>
-          </div>
+      </div>
+      <div className="flex flex-col justify-between pt-5 pb-10 border-t sm:flex-row">
+        <p className="text-sm text-gray-600">
+          © Copyright 2022 {SITE_INFOS.title}. All rights reserved.
+        </p>
+        <div className="flex items-center mt-4 space-x-4 sm:mt-0">
+          <Link href="/">
+            <a className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400">
+              <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
+              </svg>
+            </a>
+          </Link>
+          <Link href="/">
+            <a className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400">
+              <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
+                <circle cx="15" cy="15" r="4" />
+                <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
+              </svg>
+            </a>
+          </Link>
+          <Link href="/">
+            <a className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400">
+              <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
+              </svg>
+            </a>
+          </Link>
         </div>
       </div>
     </div>
--- client/pages/auth/signin.tsx
+++ client/pages/auth/signin.tsx
@@ -6,40 +6,59 @@ export default function Signin({
   providers: { name: string; id: string }[];
 }) {
   return (
-    <>
-      <div className="grid lg:grid-cols-2 min-h-screen dark:bg-deep-purple-900 dark:text-white">
-        <div className="bg-gray-100 dark:bg-deep-purple-600 hidden lg:grid place-items-center relative">
-          <div className="h-64 w-64 dark:bg-white bg-gray-900 rounded-full" />
-          <div className="absolute bottom-0 h-1/2 w-1/2 dark:bg-gray-800/50 bg-gray-100/50 backdrop-blur-lg" />
-        </div>
-        <div className="grid place-items-center">
-          <div className="max-w-sm w-full space-y-10">
-            <div className="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-white sm:px-6 md:px-8 lg:px-10">
-              <div className="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl dark:text-black">
-                Login To GitDashboard
+    <div className="flex h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="relative rounded-lg drop-shadow-lg bg-opacity-75 bg-deep-purple-accent-700 lg:-mt-28">
+        <svg
+          className="absolute inset-x-0 -bottom-1 text-white rounded-lg"
+          viewBox="0 0 1160 163"
+        >
+          <path
+            fill="currentColor"
+            d="M-164 13L-104 39.7C-44 66 76 120 196 141C316 162 436 152 556 119.7C676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C76 162.5 -44 162.5 -104 162.5H-164V13Z"
+          />
+        </svg>
+        <div className="relative px-4 py-16 mx-auto overflow-hidden sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-28">
+          <div className="flex flex-col items-center justify-between lg:flex-row">
+            <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
+              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+                Sign to Gitvision using one of our compatible providers
+              </h2>
+              <p className="max-w-xl mb-4 text-base text-gray-200 md:text-lg">
+                And access powerful insights to your git organizations right
+                now.
+              </p>
+            </div>
+            <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
+              <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
+                <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
+                  Sign in to access the dashboard
+                </h3>
+                <form>
+                  <div className="mt-4 mb-2 sm:mb-4">
+                    {Object.values(providers).map((provider) => (
+                      <div className="flex gap-4 item-center" key={provider.id}>
+                        <button
+                          type="button"
+                          className="py-2 px-4 flex justify-center items-center  bg-gray-600 hover:bg-gray-800 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                          onClick={() =>
+                            signIn(provider.id, { callbackUrl: "/dashboard" })
+                          }
+                        >
+                          {provider.name}
+                        </button>
+                      </div>
+                    ))}
+                  </div>
+                  <p className="text-xs text-gray-600 sm:text-sm">
+                    We respect your privacy. Unsubscribe at any time.
+                  </p>
+                </form>
               </div>
-              <div className="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl dark:text-black">
-                You’ll be taken to your repo provider to authenticate
-              </div>
-
-              {Object.values(providers).map((provider) => (
-                <div className="flex gap-4 item-center" key={provider.id}>
-                  <button
-                    type="button"
-                    className="py-2 px-4 flex justify-center items-center  bg-gray-600 hover:bg-gray-800 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                    onClick={() =>
-                      signIn(provider.id, { callbackUrl: "/dashboard" })
-                    }
-                  >
-                    Github
-                  </button>
-                </div>
-              ))}
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
