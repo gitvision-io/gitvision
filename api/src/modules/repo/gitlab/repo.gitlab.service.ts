cb3030f32a93964b07ee8d7043fa5567da8e9546
Chadiii <blackbeard@DESKTOP-P8DU4CN.localdomain>
2022-10-31 01:39:38 +0100

    list repo for gitlab

--- api/src/modules/repo/gitlab/repo.gitlab.service.ts
+++ api/src/modules/repo/gitlab/repo.gitlab.service.ts
@@ -4,6 +4,7 @@ import { Repo } from 'src/entities/repo.entity';
 
 import { Repository } from 'typeorm';
 import { Gitlab } from '@gitbeaker/node';
+import { ProjectSchema } from '@gitbeaker/core/dist/types/types';
 
 export interface Organization {
   id: string;
@@ -24,8 +25,8 @@ export class RepoGitlabService {
 
   auth(token: string): void {
     this.#api = new Gitlab({
-      host: 'https://gitlab.com/api/v4',
-      token,
+      host: 'https://gitlab.com',
+      oauthToken: token,
     });
   }
 
@@ -42,14 +43,34 @@ export class RepoGitlabService {
   // Get all organisations
   async getAllOrgWithPagination(): Promise<Organization[]> {
     const organizations: Organization[] = [];
-    console.log(this.#api.Groups.all({ maxPages: 50000 }));
     return organizations;
   }
 
   // Get all repositories
   async getAllRepoOfAllOrgWithPagination(): Promise<Repo[]> {
     const repositories: Repo[] = [];
+    const orgs = await this.#api.Groups.all({ maxPages: 50000 });
+    await Promise.all([
+      ...orgs.map(async (o) => {
+        if (o.id) {
+          const projects: ProjectSchema[] = await this.#api.Groups.projects(
+            o.id,
+          );
+          if (projects) {
+            projects.map((p) => {
+              if (p.id != null) {
+                const repo: Repo = new Repo();
+                repo.id = p.id.toString();
+                repo.name = p.name;
+                repo.organization = o.name;
 
+                repositories.push(repo);
+              }
+            });
+          }
+        }
+      }),
+    ]);
     return repositories;
   }
 
