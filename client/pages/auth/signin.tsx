770d7557f549dc10f1b9abee29e3bc168e9ad077
Guillaume <guillaume.jacquart@abtasty.com>
2022-11-03 18:18:15 +0100

    Improve signin

--- client/pages/auth/signin.tsx
+++ client/pages/auth/signin.tsx
@@ -1,10 +1,52 @@
 import { getProviders, signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import Button from "../../components/common/Button";
+
+const classes: Record<string, string> = {
+  GitHub:
+    "text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mb-2",
+};
+
+const icons: Record<string, JSX.Element> = {
+  GitHub: (
+    <svg
+      className="mr-2 -ml-1 w-4 h-4"
+      aria-hidden="true"
+      focusable="false"
+      data-prefix="fab"
+      data-icon="github"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 496 512"
+    >
+      <path
+        fill="currentColor"
+        d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+      ></path>
+    </svg>
+  ),
+  GitLab: (
+    <svg className="mr-2 -ml-2 -mr-2 w-6 h-6" viewBox="0 0 380 380">
+      <g id="LOGO">
+        <path
+          fill="currentColor"
+          d="M282.83,170.73l-.27-.69-26.14-68.22a6.81,6.81,0,0,0-2.69-3.24,7,7,0,0,0-8,.43,7,7,0,0,0-2.32,3.52l-17.65,54H154.29l-17.65-54A6.86,6.86,0,0,0,134.32,99a7,7,0,0,0-8-.43,6.87,6.87,0,0,0-2.69,3.24L97.44,170l-.26.69a48.54,48.54,0,0,0,16.1,56.1l.09.07.24.17,39.82,29.82,19.7,14.91,12,9.06a8.07,8.07,0,0,0,9.76,0l12-9.06,19.7-14.91,40.06-30,.1-.08A48.56,48.56,0,0,0,282.83,170.73Z"
+        />
+      </g>
+    </svg>
+  ),
+};
 
 export default function Signin({
   providers,
 }: {
   providers: { name: string; id: string }[];
 }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const { redirectUrl } = router.query;
+
   return (
     <div className="flex h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="relative rounded-lg drop-shadow-lg bg-opacity-75 bg-deep-purple-accent-700 lg:-mt-28">
@@ -37,23 +79,28 @@ export default function Signin({
                   <div className="mt-4 mb-2 sm:mb-4">
                     {Object.values(providers).map((provider) => (
                       <div className="flex gap-4 item-center" key={provider.id}>
-                        <button
+                        <Button
                           type="button"
+                          isLoading={isLoading}
                           className={`my-2 py-2 px-4 flex justify-center items-center  ${
-                            provider.name == "GitHub"
-                              ? "bg-gray-600 hover:bg-gray-800"
-                              : "bg-orange-600 hover:bg-orange-800"
+                            classes[provider.name] ||
+                            "bg-orange-600 hover:bg-orange-800"
                           } focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg`}
-                          onClick={() =>
-                            signIn(provider.id, { callbackUrl: "/dashboard" })
-                          }
+                          onClick={() => {
+                            setIsLoading(true);
+                            signIn(provider.id, {
+                              callbackUrl:
+                                (redirectUrl as string) || "/dashboard",
+                            });
+                          }}
                         >
+                          {icons[provider.name]}
                           {provider.name}
-                        </button>
+                        </Button>
                       </div>
                     ))}
                   </div>
-                  <p className="text-xs text-gray-600 sm:text-sm">
+                  <p className="text-xs text-gray-600 sm:text-xs">
                     We respect your privacy. Unsubscribe at any time.
                   </p>
                 </form>
